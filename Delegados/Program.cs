622c663da0e18962739f481ff76131a2e8fbd85c
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2020-04-24 14:26:28 -0500

    delegados y multihilos

---
+++ Delegados/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Delegados
+{
+    class Program
+    {
+        delegate int BuscarMayorNumero(List<int> lst);
+        static int BuscandoNemo(List<int> lst_)
+        {
+            return lst_.OrderByDescending(d => d).First();
+        }
+        static void Main(string[] args)
+        {
+            BuscarMayorNumero fn1 = BuscandoNemo;
+
+            Console.WriteLine(fn1(new List<int> { 1, 2, 3, 4, 5,888,8,9,5 }));
+
+        }
+    }
+}
---
+++ Multihilos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Multihilos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instaciamos el objeto thread / hilo y
+            //lo colocamos el delegado que corresponde
+            Thread miHilo = new Thread(Saludos);
+            //ahora iniciamos 
+            //ya que no arranca al instanciarse
+            miHilo.Start();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Saludos desde main");
+        }
+
+        static void Saludos()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Saludos desde el hilo");
+        }
+    }
+}
