9e971a8cdb34b2b79fcf186e5c4113beb41941f8
John Alexander Jimenez Morales <jhonalexjm@gmail.com>
2020-04-24 08:47:16 -0500

    ejercicios linq

---
+++ Linq/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 1, 2, 4, 5, 7, 5, 5, 3, 5 };   
+            //hacemos el query
+            var valores = from n in numeros
+                          where n % 2 == 0
+                          select n;
+            
+            //mostramos resultado
+            foreach(var item in valores)
+            {
+                Console.WriteLine(item);
+            }
+            //guardamos como arreglo
+            int[] arrayValores = (from n in numeros
+                                  where n % 2 == 0
+                                  select n).ToArray<int>();
+            //guardamos como lista
+            List<int> listValores = (from n in numeros
+                                  where n % 2 == 0
+                                  select n).ToList<int>();
+            //mostramos
+            Console.WriteLine("El arreglo");
+            //mostramos los resultados
+            foreach (int num in arrayValores)
+                Console.WriteLine(num);
+
+            numeros[0] = 28;
+            Console.WriteLine("---  Se actualiza despues de la modificacion");
+            //Mostramos los resultado
+            foreach (int num in arrayValores)
+                Console.WriteLine(num);
+            Console.WriteLine("La lista");
+            Console.WriteLine("---------------");
+            foreach (int num in listValores)
+                Console.WriteLine(num);
+
+
+
+        }
+    }
+}
---
+++ Linq2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creamos lista
+            List<CEstudiante> estudiantes = new List<CEstudiante>
+            { 
+                new CEstudiante("ana", "Afghj", "Mercadotecnia", 10.0),
+                new CEstudiante("mariana", "0987654", "Mercadotecnia",6.0),
+                new CEstudiante("luis", "Afghj", "Mercadotecnia", 5.0),
+                new CEstudiante("fernando", "0987654", "Mercadotecnia", 1.0)
+            };
+
+            var reprobados = from e in estudiantes
+                             where e.Promedio <= 5.0
+                             select e;
+            //Mostramos los reprobados
+            Console.WriteLine("Reprobados");
+            foreach (CEstudiante r in reprobados)
+            {
+                Console.WriteLine(r);
+            }
+            //Mostramos solo un atributo
+            Console.WriteLine("Reprobados Solo un atributo");
+            foreach (CEstudiante r in reprobados)
+            {
+                Console.WriteLine(r.Nombre);
+            }
+            //encontramos solo los nombres de los de mercadotecnia
+            var mercadotecnia = from e in estudiantes
+                                 where e.Curso == "Mercadotecnia"
+                                 select e.Nombre;
+            //Mostramos los reprobados
+            Console.WriteLine("Los de mercado tecnia");
+            foreach (string n in mercadotecnia)
+                Console.WriteLine(n);
+        }
+    }
+}
